c6c7fafe775ae15ef96e031e25a0161fc4ec5043
deadlyunicorn <retroalex1008@gmail.com>
2023-11-13 03:57:13 +0200

    user fetch update

--- src/api/users/get.ts
+++ src/api/users/get.ts
@@ -1,29 +1,41 @@
 import { Request, Response } from "express";
 import { client } from "../../db/connection"
 import { ObjectId } from "mongodb";
+import { ErrorMessages } from "../../../types";
 
 export const getUser = async( req: Request, res: Response ) => {
 
   const params = req.params as usersParams;
   const userId = params.userId;
 
-  const accounts = client.db('path_finders').collection('accounts');
-  const data = await accounts  
-  .findOne({
-    userId: userId
-  }) as userResponse;
+  const userLocations = client.db('path_finders').collection('userLocations');
 
-  if ( data ){
-    const { _id, ...finalData } = data;
-    res.json( { data: finalData } );
+  try{
+    const userIdValue = parseInt( userId );
+    if ( Number.isNaN ( userIdValue ) ){
+      throw ErrorMessages.InvalidRequest
+    }
+    const data = await userLocations  
+    .findOne({
+      userId: userIdValue
+    }) as userResponse;
+
+    if ( data ){
+      const { _id, ...finalData } = data;
+      res.json( { data: finalData } );
+    }
+    else{
+      throw 'User not found';
+    }
+
   }
-  else{
+  catch( err ){
     res.json( {
-      error: {
-        message: 'User not found'
+      error:{
+        message: err
       }
     } )
-  }
+  } 
   
 }
 
